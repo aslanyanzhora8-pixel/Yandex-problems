4e2d3953b74faa3384db4a03c82d76f267713861
Zhora <aslanyanzhora8@gmail.com>
2026-05-14 18:45:36 +0400

    Update tribonacci.py

--- tribonacci.py
+++ tribonacci.py
@@ -4,20 +4,13 @@ c = 1
 
 
 def tribonacci(n):
-    global a, b, c
     if n == 0:
         return 0
-    elif n == 1 or n == 2:
+    elif n == 1:
+        return 0
+    elif n == 2:
         return 1
-    else:
-        i = 2
-        if n >= i:
-            i += 1
-            d = a + b + c
-            a = b
-            b = c
-            c = d
-            return a + b + tribonacci(i)
+    return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
 
 
 print(tribonacci(3))
